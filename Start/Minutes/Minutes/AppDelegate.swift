f4ee189e33edacd09711e4a31a9b0254bc7fd7e8
Ryan Stroud <ryanstroud75@gmail.com>
2019-09-10 22:50:10 +0800

    Test build.

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -9,6 +9,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
+        // A comment
+        
         return true
     }
 
